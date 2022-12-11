8969149e678295a8d4bd14e963f6ba8410824634
hadleydalt <hadley_dalton@brown.edu>
2022-12-11 14:12:20 -0500

    hi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import WebFont from 'webfontloader';
+import { ExpandingPhoto } from './ExpandingPhoto'
 
 function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Work Sans']
+      }
+    });
+   }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <div style ={{backgroundColor: '#FFB0E2', borderRadius: 40, height: '85vh', width: '80vw', filter: 'drop-shadow(0px 0px 50px rgba(0, 0, 0, 0.15))'}} />
+          <div style ={{position: 'absolute', backgroundColor: 'white', borderRadius: 40, height: '85vh', width: '74vw', overflow: 'scroll'}}>
+
+            <div style={{paddingLeft: '4vw', paddingRight: '4vw'}}>
+
+              <div style={{fontSize: '1.1vw', fontFamily: 'Work Sans', paddingTop: '2vw', paddingLeft: '0.2vw'}}>Brown University CSCI 1300</div>
+              <div style={{fontSize: '2.5vw', fontFamily: 'Work Sans', paddingTop: '0.2vw'}}>
+                Responsive <b>Redesign</b>✏️
+              </div>
+
+              <div class="spacer" />
+
+              <div style={{fontSize: '1.5vw', fontFamily: 'Work Sans'}}>🔧 Assignment Overview</div>
+
+              <div class="m-spacer" />
+
+              <div style={{fontSize: '0.9vw', lineHeight: '1.3vw', fontFamily: 'Work Sans'}}>
+                The goal of this assignment was to practice the workflow of <acc>redesigning a simple website</acc>. The process involved <acc>analyzing and identifying flaws</acc> in an existing interface, creating <acc>low-fidelity</acc> and <acc>high-fidelity prototypes</acc> for various screen sizes, and <acc>building a responsive website</acc> based on those prototypes.
+              </div>
+
+              <div class="m-spacer" />
+
+              <div style={{fontSize: '1.5vw', fontFamily: 'Work Sans'}}>🔍 Part 1: Identifying Usability Problems</div>
+
+              <div class="m-spacer" />
+
+              <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start', height: '30vh'}}>
+                <div style={{display: 'flex', flexDirection: 'column'}}>
+                <div style={{width: '17vw', padding: '1vw', border: '1px solid black', borderRadius: 20}}>
+                  <div style={{fontSize: '1.15vw', fontFamily: 'Work Sans'}}>➡️ Picking a Webpage</div>
+
+
+                  <div style={{fontSize: '0.8vw', lineHeight: '1.2vw', fontFamily: 'Work Sans'}}>
+                    <p>I chose the website of the popular retail fashion company, <pacc>Zara</pacc>. You can experience the website for yourself at <a href="http://zara.com/us">zara.com</a>.</p>
+
+                    
+                    <p>I chose Zara's website because it is so difficult to navigate and use that even though I am in love with Zara clothing, I would never buy it from their website. I don't know how, even though that is the purpose of the website. </p>
+
+                      
+                  </div>
+                </div>
+                <ExpandingPhoto 
+                  text="Click for a screenshot of Zara's site" 
+                  width="17vw"
+                  clickedHeight="60vh"
+                  clickedWidth="50vw"
+                  clickedMarginTop="-50vh"
+                  imgHeight="50vh"
+                  imgWidth="45vw"
+                />
+                </div>
+                <div style={{width: '17vw', padding: '1vw', border: '1px solid black', borderRadius: 20}}>
+                  <div style={{fontSize: '1.15vw', fontFamily: 'Work Sans'}}>➡️ Finding Problems</div>
+                </div>
+                <div style={{width: '17vw', padding: '1vw', border: '1px solid black', borderRadius: 20}}>
+                  <div style={{fontSize: '1.15vw', fontFamily: 'Work Sans'}}>➡️ Accessibility</div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
     </div>
   );
 }
---
+++ src/ExpandingPhoto.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export const ExpandingPhoto = (props) => {
+    const [clicked, setClicked] = React.useState(false)
+
+    const unclickedStyle = {
+        backgroundColor: '#FFB0E2', height: '0.8vw', width: '0.8vw', borderRadius: '0.8vw', marginLeft: '0.3vw', border: 'none'
+    }
+
+    const clickedStyle = {
+        backgroundColor: '#FFB0E2', height: props.clickedHeight, width: props.clickedWidth, borderRadius: '2vw', marginLeft: '0.3vw', border: 'none', marginTop: props.clickedMarginTop, position: 'absolute', filter: 'drop-shadow(0px 0px 30px rgba(0, 0, 0, 0.3))'
+    }
+
+
+    return (
+    <div>
+        <button
+        style={{border: 0, background: 'transparent'}}
+        onClick={() => {
+            if (clicked) {
+                setClicked(false)
+            } else {
+                setClicked(true)
+            }
+            
+        }}>
+        <div style={{display: 'flex', alignItems: 'center', flexDirection: 'row', width: props.width, fontSize: '0.9vw', fontFamily: 'Work Sans', padding: '0.5vw'}}>
+            {props.text}
+            <div 
+                style={clicked ? clickedStyle : unclickedStyle}
+            >
+                {clicked ? 
+                <div style={{display: 'flex', alignItems: 'flex-start', justifyContent: 'flex-start'}}><div style={{color: 'white', padding: '1vw', fontSize: '1vw'}}>✖️</div>
+                <img style={{height: props.imgHeight, width: props.imgWidth, paddingTop: "5vh"}} src={require("./images/zara.png")} /></div> : null
+                }
+            </div>
+        </div>
+        </button>
+    </div>
+    )
+}
