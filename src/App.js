e4d5159f0f059385412d9964b4add802a5fe0cce
hadleydalt <hadley_dalton@brown.edu>
2022-12-11 15:40:49 -0500

    hi

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
 
               <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start', height: '30vh'}}>
                 <div style={{display: 'flex', flexDirection: 'column'}}>
-                <div style={{width: '17vw', padding: '1vw', border: '1px solid black', borderRadius: 20}}>
+                <div style={{width: '17vw', padding: '1vw', border: '1px solid black', borderRadius: 20, height: '30vh', overflow: 'scroll'}}>
                   <div style={{fontSize: '1.15vw', fontFamily: 'Work Sans'}}>➡️ Picking a Webpage</div>
 
 
@@ -66,11 +66,69 @@ function App() {
                   imgWidth="45vw"
                 />
                 </div>
-                <div style={{width: '17vw', padding: '1vw', border: '1px solid black', borderRadius: 20}}>
+                <div style={{width: '17vw', padding: '1vw', border: '1px solid black', borderRadius: 20, height: '30vh', overflow: 'scroll'}}>
                   <div style={{fontSize: '1.15vw', fontFamily: 'Work Sans'}}>➡️ Finding Problems</div>
+
+                  <div style={{fontSize: '0.8vw', lineHeight: '1.2vw', fontFamily: 'Work Sans'}}>
+
+                    
+                    <p><i><b>Usability.</b></i> The capability of the software product to be 
+<pacc>understood</pacc>, <pacc>learned</pacc>, <pacc>used</pacc> and <pacc>attractive</pacc> to the 
+user, when used under specified conditions.</p>
+
+<div style={{fontSize: '0.8vw', lineHeight: '1.2vw', fontFamily: 'Work Sans'}}>
+<p style={{background: '#FFE7F6'}}>
+                    Unmet Expectations
+                    </p>
+                    Upon entering the site, the user is immediately bombarded with photos of child models. This may conflict with the user's preconceived notion of the site, as typically, clothing sites list retail items on the first page rather than flooding it with visuals. 
+                    <p style={{background: '#FFE7F6'}}>
+                    Learning Obstacles 
+                    </p>
+                    The website navigation is counterintuitive. At the bottom of the screen the user is presented links to categories such as "girl", "boy", etc. Instead of actually leading the user to item listings, these links only serve to change which images that are being projected.
+                    <p style={{background: '#FFE7F6'}}>
+                    Inconsistency 
+                    </p>
+                    When the user finally figures out how to view item listings (through accessing the main menu by clicking a tiny icon in the top left corner), the website presents listings inconsistently. In some cases, the names of the items are listed below the item. In other cases, they are not. There is no apparent reasoning or pattern. 
+                    <p style={{background: '#FFE7F6'}}>
+                    Confusing Queues
+
+                    </p>
+                    Link titles within the menu in  are confusing or misleading. Examples include "Join Life". There is no telling what this could possibly mean. 
+                    <p style={{background: '#FFE7F6'}}>
+                    Cryptic Abbreviations 
+                      </p>
+                      Zara employs some jargon in their menu link titles. Whatever "Zara Srpls" means is unclear to the user. If it means "Surplus", there was definitely room to write that entire word. After clicking this link, the user is lead to a page with shots of models wearing clothing that is presumably Zara. This has nothing to do with a "surplus".
+
+                      <p style={{background: '#FFE7F6'}}>
+                    Low Contrast
+                      </p>
+                      In many cases, the website's text (for example, navigation links or menu links) fall directly on top of an image. The text is always black, and sometimes the images are dark in color. This presents a problem as the text is difficult to read, leading to confusion and unattractiveness.
+                  </div>
+                  </div>
                 </div>
-                <div style={{width: '17vw', padding: '1vw', border: '1px solid black', borderRadius: 20}}>
+                <div style={{width: '17vw', padding: '1vw', border: '1px solid black', borderRadius: 20, height: '30vh', overflow: 'scroll'}}>
                   <div style={{fontSize: '1.15vw', fontFamily: 'Work Sans'}}>➡️ Accessibility</div>
+                  <div style={{fontSize: '0.8vw', lineHeight: '1.2vw', fontFamily: 'Work Sans'}}>
+                  <p>
+                    <a href="https://wave.webaim.org/">WebAim WAVE</a> was used to detect accessibility problems with the website. Flagged features included: 
+                  </p>
+                  <p style={{background: '#FFE7F6'}}>
+                    Missing alternative text
+                  </p>
+                  Correct, there are no textual alternatives for images. So, the website cannot be screenread to a visually impaired person.
+                  <p style={{background: '#FFE7F6'}}>
+                    Low contrast
+                  </p>
+                  Correct, text blends in with the images in many cases.
+                  <p style={{background: '#FFE7F6'}}>
+                    Skipped heading levels
+                  </p>
+                  Correct, there does not seem to be an intermediary heading size between the huge title text and any other text on the website (any other text besides the title is tiny).
+                  <p style={{background: '#FFE7F6'}}>
+                    Very small text
+                  </p>
+                  Correct, all text that is not the main heading is miniscule. However, enlarging the website using "Command/Control" + "+" on a keyboard does enlarge the text. 
+                </div>
                 </div>
               </div>
             </div>
